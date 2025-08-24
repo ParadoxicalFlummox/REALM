190f6642e72d007398c48d75935c747f7a9a3582
ParadoxicalFlummox <adamr814@protonmail.com>
2025-08-24 10:55:51 -0500

    Update models to use gorm; add relevant database properties

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,7 +1,13 @@
 package models
 
+import (
+	"time"
+)
 type User struct {
-	ID				uint
-	Email			string
-	Password		string
+	ID       	uint			`gorm:"primaryKey"`
+	Email    	string			`gorm:"unique;not null"`
+	Password 	string			`gorm:"not null"`
+	Properties 	[]Property 		`gorm:"foreignKey:OwnerID"`
+	CreatedAt 	time:Time
+	UpdatedAt 	time:Time
 }
